80ef7c6473aefc58576e65d8e1b655c9904d4f52
Yasin Silavi <ys.silavi@gmail.com>
2022-02-25 14:39:50 +0330

    feat: add routes

--- server.js
+++ server.js
@@ -11,15 +11,20 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get("/", function (req, res) {
+// Routes
+
+app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
+app.post("/api/shorturl", (req, res) => {
   res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function () {
+app.get("/api/shorturl", (req, res) => {
+  res.json({ greeting: "hello API" });
+});
+
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
