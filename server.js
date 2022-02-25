ac560a4df26ba535dbec86bbbf275d9b7fc1bd61
Yasin Silavi <ys.silavi@gmail.com>
2022-02-25 12:38:04 +0330

    style: format server.js file with prettier

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -9,17 +9,17 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
